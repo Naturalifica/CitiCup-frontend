914a80681a98cc1cbd7ad76a482eb270f689897d
Ruilin Geng <1585363499@qq.com>
2022-12-28 13:18:53 +0800

    吴佬救救

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,8 +7,10 @@
       </el-header>
       <el-main>
         <Carousel/>
+        <div style="height: 30px; line-height: 70px; display: flex"/>
+          <div class="font_div">关于我们</div>
       </el-main>
-      <el-footer>Footer</el-footer>
+      <el-footer></el-footer>
     </el-container>
   </div>
 
@@ -29,5 +31,10 @@ export default {
 <style scoped>
 .bk{
   background-color: #303133;
+}
+.font_div{
+  font-weight:bold;/*加粗*/
+  text-align:center;   /* 水平居中 */
+  color: #c8cfd8;
 }
 </style>
